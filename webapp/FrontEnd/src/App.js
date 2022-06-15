be23df9bd375af3c5415d440397b131f787ee348
Konrad Hilse <khilse@psu.edu>
2022-06-15 12:17:21 -0400

    Remove warnings

--- webapp/FrontEnd/src/App.js
+++ webapp/FrontEnd/src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import SearchEngine from './Components/SearchEngine';
 import GoogleMap from './Components/GoogleMap';
