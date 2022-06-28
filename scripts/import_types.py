1c72e15faf0af0b2940719945b5353a0e6614d84
Konrad Hilse <rkh24@psu.edu>
2022-06-28 10:33:57 -0400

    Fixed script for loading types

--- scripts/import_types.py
+++ scripts/import_types.py
@@ -6,6 +6,9 @@ from requests.auth import HTTPBasicAuth
 import requests
 import config.instool as instool
 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 def create_json(name, label, category):
     json_dict = {
         "name": name,
@@ -32,14 +35,12 @@ with open('data/types.csv', encoding='utf-8-sig') as csvfile:
 
         data = create_json(name, label, currentCategories[level-1] )
 
+        headers['X-API-Key'] = instool.auth
         result = requests.post(instool.url + '/instrument-types', json=data, headers=headers, auth=HTTPBasicAuth('X-API-Key', instool.auth), verify=False)
-        if result.status_code == 201:
-            try:
-                result = json.load(result)
-            except:
-                print(result.text)
+        if result.status_code == 201 or result.status_code == 200:
+                print('Sucessful')
         else: 
-                print(result.text)
+                print('Error {}: {}'.format(result.status_code, result.text))
 
 
 
