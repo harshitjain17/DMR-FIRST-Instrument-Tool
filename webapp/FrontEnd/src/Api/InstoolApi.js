26113a8a6159c8c36d81ce5d00bef0e483299d6f
Konrad Hilse <rkh24@psu.edu>
2022-07-14 09:30:24 -0400

    Fix accidentally checked in API url

--- webapp/FrontEnd/src/Api/InstoolApi.js
+++ webapp/FrontEnd/src/Api/InstoolApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: `https://m4-instool.vmhost.psu.edu/api/v1`  
-  baseURL: `https://localhost:44322/api/v1`  
+  baseURL: `https://m4-instool.vmhost.psu.edu/api/v1`  
+  //baseURL: `https://localhost:44322/api/v1`  
 });
