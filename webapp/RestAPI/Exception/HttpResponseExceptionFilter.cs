dbdccdc7810a5c1d5d28e0143b980af296c2bf3e
Konrad Hilse <rkh24@psu.edu>
2022-08-03 12:06:05 +0200

    Handle exception calling api methods

--- webapp/RestAPI/Exception/HttpResponseExceptionFilter.cs
+++ webapp/RestAPI/Exception/HttpResponseExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Instool.RestAPI.Exceptions
@@ -18,6 +19,13 @@ namespace Instool.RestAPI.Exceptions
                     StatusCode = httpResponseException.StatusCode
                 };
 
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception is not null)
+            {
+                // Only using API calls to the backend. This assures that dotnet returns an error,
+                // instead of showing the main application site.
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
                 context.ExceptionHandled = true;
             }
         }
