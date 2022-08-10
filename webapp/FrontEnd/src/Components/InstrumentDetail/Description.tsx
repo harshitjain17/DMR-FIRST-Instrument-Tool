f8d9509d47ee80835a96a71bab9feaa53d8167af
harshitjain17 <harshitj.cs@gmail.com>
2022-08-10 15:44:54 -0400

    Instrument Page Formatting

--- webapp/FrontEnd/src/Components/InstrumentDetail/Description.tsx
+++ webapp/FrontEnd/src/Components/InstrumentDetail/Description.tsx
@@ -32,7 +32,7 @@ export default function Description({ description, capabilities, image }: Descri
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: '55vh',
+                    height: '50vh',
                     backgroundColor: (theme) =>
                         theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
                 }}
@@ -40,12 +40,12 @@ export default function Description({ description, capabilities, image }: Descri
                 <Grid container spacing={1}>
                     {image &&
                         <Grid item xs={4} md={4} lg={4}>
-                            <ButtonBase sx={{ width: '100%', height: '50vh' }}>
+                            <ButtonBase sx={{ width: '100%', height: '45vh' }}>
                                 <Img alt="Image Not Found" src={`${config.url}${image}`} />
                             </ButtonBase>
                         </Grid>}
                     <Grid item xs={image ? 8 : 12} md={image ? 8 : 12} lg={image ? 8 : 12} sm container>
-                        <div style={{ height: '50vh', overflowY: 'auto' }}>
+                        <div style={{ height: '44vh', overflowY: 'auto' }}>
                             <Typography variant="body2" align='justify' component="div">
                                 <ReactMarkdown rehypePlugins={[rehypeRaw]} children={description ?? ""} />
                             </Typography>
