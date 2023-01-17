3b1a95ab341af9b2bfb64d6a5c2aef2c6a2d0bcc
Harshit Jain <harshitj.cs@gmail.com>
2023-01-17 10:24:22 -0500

    research expert names hyperlink

--- webapp/FrontEnd/src/Components/InstrumentDetail/ContactsTab.tsx
+++ webapp/FrontEnd/src/Components/InstrumentDetail/ContactsTab.tsx
@@ -32,7 +32,7 @@ export function ContactsTab({ contacts }: ContactsTabProps) {
                             key={row?.email}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
-                            <TableCell align="center" component="th" scope="row"><Typography variant="body2" gutterBottom component="div">{row?.firstName} {row?.middleName} {row?.lastName}</Typography></TableCell>
+                            <TableCell align="center" component="th" scope="row"><Link href={"https://dev.mri.psu.edu/mri/personnel-directory/" + row?.email.replace("@psu.edu","")} target="_blank"  rel="noopener"><Typography variant="body2" gutterBottom component="div">{row?.firstName} {row?.middleName} {row?.lastName}</Typography></Link></TableCell>
                             <TableCell align="center"><Link href="mailto:" target="_blank"  rel="noopener"><Typography variant="body2" gutterBottom component="div">{row?.email ?? row?.eppn}</Typography></Link></TableCell>
                             <TableCell align="center"><Typography variant="body2" gutterBottom component="div">{row?.role}</Typography></TableCell>
                         </TableRow>
